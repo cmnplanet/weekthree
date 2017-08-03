970d1f4e0ee1846accfd5a72559b9588e09a42c1
cmnplanet <cmnfv@mac.com>
2017-08-03 08:59:04 -0400

    second commit

--- main.js
+++ main.js
@@ -23,108 +23,113 @@ function question1 () {
     average = add / price.length;
     jFix = average.toFixed(2); //two places -- understand the toFixed?
   }
-  return(jFix);
+  return jFix;
 }
 
 console.log("The average price is " + question1()) ;
 
-// // // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
-function question2 () {
-//   // Answer:
-
-  let newArray = [];
-
-  for( let i = 0; i < data.length; i++ ){
-    if (data[i].price >= 14 && data[i].price <= 18);
-    newArray.push (data[i].title);
-  }
-  return(newArray);
-}
-
-console.log(question2());
+// // // // 2: Show me how to get an array of items that cost between $14.00 and $18.00 USD
+// function question2 () {
+// //   // Answer:
+//
+//   let newArray = [];
+//
+//   for( let i = 0; i < data.length; i++ ){
+//     if (data[i].price >= 14 && data[i].price <= 18);
+//     newArray.push (data[i].title);
+//   }
+//
+//   return newArray;
+// }
+//
+// console.log(question2());
 //
 // // data.price >= 14
 // // data.price <= 18
 //
 //
-// // // 3: Which item has a "GBP" currency code? Display it's name and price.
-function question3 () {
-  // Answer:
-
-let knewArray =[];
-for( let i = 0; i < data.length; i++ ){
-  if (data[i].currency_code === "GBP");
-    knewArray.push (data[i].name);
-  }
-
-}
-
-console.log(question3 (knewArray.name + " costs " + knewArray.price));
-//
-// //
-// // // // data.currency_code = GBP [11]
-// // //
-// // //
-// // // 4: Display a list of all items who are made of wood.
-function question4 () {
+// // // // 3: Which item has a "GBP" currency code? Display it's name and price.
+// function question3 () {
 //   // Answer:
 //
-  let woodContainer [];
-  let x [];
-
-  for ( let i = 0; i = data.length; i++){
-    woodContainer.push (data[i].materials === "wood");
-}
-  for ( let j = 0; j = woodContainer.length; j++){
-    if { ( [i] [j])
-
-    }
-
-}
+// let knewArray =[];
+// for( let i = 0; i < data.length; i++ ){
+//   if (data[i].currency_code === "GBP");
+//     knewArray.push (data[i].name);
+//   }
 //
+// return knewArray;
 // }
 //
+// console.log(question3 (knewArray.title + "costs" + knewArray.price));
 // // //
-// // //
-// // 5: Which items are made of eight or more materials?
-// //    Display the name, number of items and the items it is made of.
-function question5 () {
-  // Answer:
-
-  // let moreMaterials = [];
-  // let count = 0;
-
-  for (let i = 0; i < data.length; i++){
-    const item = data[i];
-    if (item.materials.length >7 );
-     console.log (`${item.title} has ${item.materials.length}:`);
-
-        for (let j = 0; j = item.materials.length; j++){
-        // moreMaterials.push (data[i].materials);
-        const material = item.materials[j];
-        console.log("- " + material)
-
-}
-
-// >7
-
-console.log (question5())
+// // // //
+// // // // // // data.currency_code = GBP [11]
 // // // // //
-// 6: How many items were made by their sellers?
-// // Answer:
-function question6 () {
-  // Answer: if true
-
-  let = items [];
-  let = count = 0;
-
-  for ( let i = 0; i = data.length; i++){
-    if (data[i].who_made === "i_did");
-        items.push(data[i].who_made);
-        // count =+;
-        count = items.length;
-  }
-  return (count);
-}
-
-console.log(question6()count + "items were made by their sellers\.");
+// // // // //
+// // // 4: Display a list of all items who are made of wood.
+// function question4 () {
+// //   // Answer:
+//   let woodContainer =[];
+//   let sampling =[];
+//
+//   for ( let i = 0; i < data.length; i++){
+//     woodContainer.push (data[i].materials === "wood");
+//
+//   for ( let j = 0; j < woodContainer.length; j++){
+//     if (woodContainer [i] [j]);
+//       sampling.push(data[i].length);
+//
+//     }
+// return sampling;
+// }
+//
+//
+// console.log(question4());
+// // //
+// // //
+// // // // //
+// // // // //
+// // // 5: Which items are made of eight or more materials?
+// // //    Display the name, number of items and the items it is made of.
+// function question5 () {
+//   // Answer:
+//
+//     let item = data[i];
+//
+//   for (let i = 0; i < data.length; i++){
+//     if (item.materials.length >7 );
+//       console.log (`${item.title} has ${item.materials.length} materials:`);
+//         }
+//
+//       for (let j = 0; j < item.materials.length; j++){
+//         let sample = item.materials[i];
+//         console.log("- " + sample);
+//     }
+// }
+//
+// // let price = [];
+// // for( let i = 0; i < data.length; i++ ){
+// //   price.push (data[i].price);
+// // }
+//
+// console.log (question5());
+// // // // // // //
+// // // 6: How many items were made by their sellers?
+// // // // Answer:
+// function question6 () {
+//   // Answer: if true
+//
+//   let = items = [];
+//   let = count = 0;
+//
+//   for ( let i = 0; i = data.length; i++){
+//     if (data[i].who_made === "i_did");
+//         items.push(data[i].who_made);
+//         // count =+;
+//         count = items.length;
+//   }
+//   return count;
+// }
+//
+// console.log(question6() + "items were made by their sellers\.");
